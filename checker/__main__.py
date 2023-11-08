c4d516640a7849be1375e197860e765cc285f80d
blad64 <brunotejeria@hotmail.com>
2023-11-08 08:58:58 -0300

    Add colorama library and implement user search functionality

--- checker/__main__.py
+++ checker/__main__.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import json
 import requests
+import colorama
 
 """def open_file(file: str, mode: str ="r", data: list = None, json: bool = False):
   if mode == "r":
@@ -26,20 +27,38 @@ def main():
 
   # Archivo de texto para buscar
   #inp = input("Archivo de texto: ")
-  TEXT_FILE = f"../text_files/text"
+  TEXT_FILE = f"./text_files/text"
   if ".txt" not in TEXT_FILE:
     TEXT_FILE += ".txt"
 
   # Archivo de texto para guardar
   #out = input("Archivo de salida: ")
-  RESULT_FILE = f"../results/r.txt"
+  RESULT_FILE = f"./results/r.txt"
 
   # Leer configuracion
-  config = read_config()
+  #config = read_config()
+
+
 
   # Leer archivo de texto
   with open(TEXT_FILE, "r") as f:
     text = f.readlines()
-    print(text)
+
+    founded = np.array([])
+
+    for user in text:
+      # sacar '\n'
+      username = user.split(":")[0]
+      url = f"https://www.roblox.com/user.aspx?username={username}"
+      response = requests.get(url)
+      if response.status_code == 200:
+        np.append(founded, user)
+        print(Fore.GREEN +f"Found {username}")
+  with open(RESULT_FILE, "w") as f:
+    for user in founded:
+      print(user)
+      f.write(user)
+
+main()
 
 
