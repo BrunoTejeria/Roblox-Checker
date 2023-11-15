353fbc5cb9aa0c182db77f8453532ab921358101
blad64 <brunotejeria@hotmail.com>
2023-11-15 19:15:09 -0300

    - Cambios en funcion main

--- checker/__main__.py
+++ checker/__main__.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import json
 import requests
+import tqdm
 from colorama import (
   Fore
 )
@@ -19,53 +20,62 @@ from colorama import (
       for line in data:
         f.write(line)"""
 
+import concurrent.futures
+
+# La función que quieres ejecutar múltiples veces
+def mi_funcion():
+    # Aquí puedes poner tu código a ejecutar
+    resultado = 42  # Ejemplo, reemplaza esto con tu código
+    return resultado
+
+
+
+
 
 def read_config():
   with open("checker/config.json", "r") as f:
     return json.load(f)
 
+def request(user: str, pbar: tqdm.tqdm = None):
+  try:
+    # Dividir el user del password
+    username = user.split(":")[0]
+    url = f"https://www.roblox.com/user.aspx?username={username}"
+    response = requests.get(url)
+    pbar.update(1)
+
+    # Si el código de respuesta es 200, entonces guardar el user
+    if response.status_code == 200:
+      print(Fore.GREEN +f"Found {username}")
+      return user
+  except Exception as e:
+    return None
 
 def main():
-
-  # Archivo de texto para buscar
-  #inp = input("Archivo de texto: ")
   TEXT_FILE = f"./text_files/text"
   if ".txt" not in TEXT_FILE:
     TEXT_FILE += ".txt"
-
-  # Archivo de texto para guardar
-  #out = input("Archivo de salida: ")
   RESULT_FILE = f"./results/r.txt"
-
-  # Leer configuracion
-  #config = read_config()
-
 
 
   # Leer archivo de texto
   with open(TEXT_FILE, "r") as f:
     text = f.readlines()
-
     founded = np.array([])
 
-
     # Buscar si los usuarios existen
-    for user in text:
-      # sacar '\n'
-      username = user.split(":")[0]
-      url = f"https://www.roblox.com/user.aspx?username={username}"
-      response = requests.get(url)
+    with tqdm.tqdm(total=len(text)) as pbar:
+      with concurrent.futures.ThreadPoolExecutor() as exe:
+        resultados = list(exe.map(request, text))
+        for i in resultados:
+          if i != None:
+            founded = np.append(founded, i)
 
-      # Si el código de respuesta es 200, entonces guardar el user
-      if response.status_code == 200:
-        np.append(founded, user)
-        print(Fore.GREEN +f"Found {username}")
 
-  # Escribir users en archivo de salida
-  with open(RESULT_FILE, "w") as f:
-    for user in founded:
-      print(user)
-      f.write(user)
+    # Escribir users en archivo de salida
+    with open(RESULT_FILE, "w") as f:
+      for user in founded:
+        f.write(user)
 
 main()
 
