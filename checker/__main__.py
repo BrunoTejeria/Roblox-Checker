d2c393f09c6ebebac47262e6385648d3d024219d
blad64 <brunotejeria@hotmail.com>
2023-11-16 18:58:32 -0300

    Add game check for Islands in user request

--- checker/__main__.py
+++ checker/__main__.py
@@ -2,6 +2,7 @@ import numpy as np
 import json
 import requests
 import tqdm
+import bs4 as bs
 from colorama import Fore
 
 import concurrent.futures
@@ -16,18 +17,29 @@ def request(user: str, pbar: tqdm.tqdm = None):
         username = user.split(":")[0]
         url = f"https://www.roblox.com/user.aspx?username={username}"
         response = requests.get(url, timeout=5)
-        
+
+        print(Fore.LIGHTYELLOW_EX + "\n\n" + "#" * 64 + "\n")
 
         # Si el código de respuesta es 200, entonces guardar el user
         if response.status_code == 200:
-            print(Fore.GREEN + f"Found {username}")
             pbar.update(1)
-            return user
+            print(Fore.GREEN + f"Found {user}")
+
+            # Buscar si se jugo el juego islands
+            soup = bs.BeautifulSoup(response.text, 'html.parser')
+
+            game = soup.find("a", {"href": "/games/4872321990/Islands"})
+            if game is None:
+                print(Fore.RED + f"Not found game {user}")
+                return [user, False]
+            else:
+                print(Fore.GREEN + f"Found game {user}")
+                return [user, True]
         else:
-          print(Fore.RED + f"Not found {username}")
-          pbar.update(1)
-          return None
-        
+            print(Fore.RED + f"Not found {user}")
+            pbar.update(1)
+            return None
+
     except Exception as e:
         return None
 
@@ -43,21 +55,15 @@ def main():
     founded = []
 
     # Buscar si los usuarios existen
-    with open(RESULT_FILE, "w") as f:
+    with open(RESULT_FILE, "a") as f:
         with tqdm.tqdm(total=len(usernames), ncols=64, bar_format='{desc}: {percentage:3.0f}% | {n_fmt}/{total_fmt} [{elapsed}<{remaining} | ', colour='red') as pbar:
 
             for user in usernames:
                 req = request(user, pbar)
                 founded.append(req)
-                if req != None:
-                    f.write(req + "\n")
-
-            for i in resultados:
-                if i is not None:
-                    founded.append(i)
-            
-
-
+                if req is not None:
+                    if req[1] == True:
+                        f.write(req + "\n")
 
         print(founded)
 
