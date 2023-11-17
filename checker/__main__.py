31c9538fb3ecad454b8dca0c32267d1ee1ec436f
blad64 <brunotejeria@hotmail.com>
2023-11-17 13:48:00 -0300

    Add import for time module and implement get_favorite_games function

--- checker/__main__.py
+++ checker/__main__.py
@@ -1,5 +1,6 @@
 import numpy as np
 import json
+import time
 import requests
 import tqdm
 import bs4 as bs
@@ -7,29 +8,61 @@ from colorama import Fore
 
 import concurrent.futures
 
+
+def get_favorite_games(user_id: str):
+    search = ["https://www.roblox.com/games/4872321990/Islands", "https://www.roblox.com/games/920587237/Adopt-Me"]
+    url = f"https://www.roblox.com/users/favorites/list-json?assetTypeId=9&itemsPerPage=999&pageNumber=1&userId={user_id}"
+    
+
+    print(Fore.GREEN + f"Found {user_id}")
+    favorites = requests.get(url).json()
+    print(favorites)
+
+    index = favorites["Data"]["TotalItems"] - 1
+    print(index)
+
+    for i in index:
+        print(favorites["Data"]["Items"][i]["Item"]["AbsoluteUrl"])
+
+
+
+
+
+
+
+
+
+
 def read_config():
     with open("checker/config.json", "r") as f:
         return json.load(f)
 
 def request(user: str, pbar: tqdm.tqdm = None):
     try:
+
+        print(Fore.LIGHTYELLOW_EX + "\n\n" + "#" * 64 + "\n")
         # Dividir el user del password
         username = user.split(":")[0]
         url = f"https://www.roblox.com/user.aspx?username={username}"
-        response = requests.get(url, timeout=5)
 
-        print(Fore.LIGHTYELLOW_EX + "\n\n" + "#" * 64 + "\n")
+        t3 = time.time()
+        response = requests.get(url)
+        t4 = time.time()
+        print(Fore.CYAN + f"Time to request: {t4 - t3}")
+
+        print(response.url)
+
+        
 
         # Si el código de respuesta es 200, entonces guardar el user
         if response.status_code == 200:
             pbar.update(1)
-            print(Fore.GREEN + f"Found {user}")
 
-            # Buscar si se jugo el juego islands
-            soup = bs.BeautifulSoup(response.text, 'html.parser')
+            # Obtener el user id
+            user_id = response.url.split("users/")[1].split("/")[0]
+            get_favorite_games(user_id)
 
-            game = soup.find("a", {"href": "/games/4872321990/Islands"})
-            if game is None:
+            if islands is None:
                 print(Fore.RED + f"Not found game {user}")
                 return [user, False]
             else:
@@ -41,17 +74,19 @@ def request(user: str, pbar: tqdm.tqdm = None):
             return None
 
     except Exception as e:
+        print(e)
         return None
 
 def main():
     TEXT_FILE: str = f"./text_files/text.txt"
     RESULT_FILE: str = f"./results/r.txt"
 
+    t1 = time.time()
     # Leer archivo de texto
     with open(TEXT_FILE, "r") as f:
-        text = f.readlines()
-        usernames = [line.split(":")[0] for line in text]
-
+        usernames = f.readlines()
+    t2 = time.time()
+    print(f"Time to read file: {t2 - t1}")
     founded = []
 
     # Buscar si los usuarios existen
@@ -65,7 +100,6 @@ def main():
                     if req[1] == True:
                         f.write(req + "\n")
 
-        print(founded)
 
 if __name__ == "__main__":
     main()
